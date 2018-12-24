b6303d4f1f35cfd4867870fbf240098d3bcc1737
Vaibhav Bali <vbali@internode.on.net>
2018-12-25 10:49:26 +1100

    Created HashSet Associative Array

--- Algorithms.ConsoleRunner/Program.cs
+++ Algorithms.ConsoleRunner/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Algorithms.Trees;
 
 namespace Algorithms.ConsoleRunner
@@ -7,31 +8,42 @@ namespace Algorithms.ConsoleRunner
    {
       static void Main(string[] args)
         {
-            var bst = new BinarySearchTree<int>();
+            //var bst = new BinarySearchTree<int>();
 
-            bst.Add(100);
-            bst.Add(50);
-            bst.Add(25);
-            bst.Add(75);
-            bst.Add(35);
-            bst.Add(90);
-            bst.Add(200);
-            bst.Add(150);
-            bst.Add(250);
-            bst.Add(225);
+            //bst.Add(100);
+            //bst.Add(50);
+            //bst.Add(25);
+            //bst.Add(75);
+            //bst.Add(35);
+            //bst.Add(90);
+            //bst.Add(200);
+            //bst.Add(150);
+            //bst.Add(250);
+            //bst.Add(225);
 
-            Console.WriteLine(bst.Contains(225));
-            Console.WriteLine(bst.Contains(400));
-            Console.WriteLine(bst.Contains(-1));
+            //Console.WriteLine(bst.Contains(225));
+            //Console.WriteLine(bst.Contains(400));
+            //Console.WriteLine(bst.Contains(-1));
 
-            bst.Remove(100);
+            //bst.Remove(100);
 
-            PrintTree(bst);
+            //PrintTree(bst);
 
-            bst.Remove(250);
+            //bst.Remove(250);
 
-            PrintTree(bst);
+            //PrintTree(bst);
 
+            var something = new Algorithms.AssociativeArrays.HashTable<int, int>(10);
+            something.Add(10, 10);
+            something.Add(20, 20);
+            something.Add(11, 23);
+
+            something.Remove(11);
+
+           
+           
+
+           
         }
 
         private static void PrintTree(BinarySearchTree<int> bst)
---
+++ AssociativeArrays/HashTable.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssociativeArrays.AssociativeArrays
+{
+   public class HashTable<TKey, TValue>
+        where TKey : IComparable<TKey>
+   {
+      internal LinkedList<TKey, TValue>[] backingStore;
+      public int Count { get; private set; }
+      public HashTable(int size)
+      {
+         backingStore = new backingStore[size];
+      }
+
+      public void Add(TKey key, TValue value)
+      {
+         var position = key.GetHashCode % backingStore.Length;
+
+         if (backingStore[position] == null)
+         {
+            backingStore[position] = new LinkedList<TKey, TValue>();
+         } 
+         else
+         {
+             //check if item is alredy there
+             if(backingStore[position].Contains(key)){
+                 throw new InvalidOperationException("Cannot add duplicate keys");
+             }
+         }
+
+         backingStore[position].AddLast(key, value);
+         ++Count;
+         
+         return;
+      }
+
+      public TValue Remove(TKey key, TValue value)
+      {
+          var position = key.GetHashCode() % backingStore.Length;
+
+          if(backingStore[position] == null || backingStore[position].First == null){
+              throw new InvalidOperationException("Key is not present");
+          } 
+
+          var potentialItem = backingStore[position].Find[key];
+          if(potentialItem.key == key && potentialItem.value == value){
+              backingStore[position].Remove(key);
+          }
+
+          --Count;
+          return potentialItem.value;
+      }
+   }
+}
